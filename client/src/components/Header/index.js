315e7844f5aa384ba550cf85fe1d6a3bcd5564bf
Nathan Chow <emailme@nathanchow.ca>
2021-05-11 16:30:59 -0400

    update profile.js

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -2,8 +2,15 @@ import React from 'react';
 import '../../css/Header.css';
 import Icon from '@iconify/react';
 import Logo from '../../assets/images/woof-logo-white.png';
+import Auth from '../../utils/auth';
+import { Link } from 'react-router-dom'
 
 function Header(props) {
+
+    const logout = event => {
+        event.preventDefault();
+        Auth.logout();
+      };
 
     const {
         links = [],
@@ -17,16 +24,25 @@ function Header(props) {
                 <img className="header-logo" src={Logo} alt=""></img>
             </div>
             <div className="nav-links-container">
+                {Auth.loggedIn() ? (
+                    <>
+                    <Link to="/#profile">Profile</Link>
+                    <a href="/" onClick={logout}>Logout</a>
+                    </>
+                ) : (
+                    <>
                 {links.map((link) => (
-                <li className="list-link">
-                    <a href={link.href} className={`link ${currentLink.name === link.name && `navActive`}`} key={link.name} onClick={() => {
-                    setCurrentLink(link);
-                    }}>{link.name}</a>
-                    <a href={link.href} className={`icon ${currentLink.name === link.name && `navActive`}`} key={link.name} onClick={() => {
-                    setCurrentLink(link);
-                    }}><Icon icon={link.icon}/></a>
-                </li>
-                ))}
+                    <li className="list-link">
+                        <a href={link.href} className={`link ${currentLink.name === link.name && `navActive`}`} key={link.name} onClick={() => {
+                        setCurrentLink(link);
+                        }}>{link.name}</a>
+                        <a href={link.href} className={`icon ${currentLink.name === link.name && `navActive`}`} key={link.name} onClick={() => {
+                        setCurrentLink(link);
+                        }}><Icon icon={link.icon}/></a>
+                    </li>
+                    ))}
+                    </>
+                )}
             </div>
         </nav>
     )
