45c3c9aa3d357a099e04687b9487fd3ec894986f
unknown <km_si@ymail.com>
2021-05-11 13:51:29 -0400

    fixing some errors

--- server/models/Walker.js
+++ server/models/Walker.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const { Schema } = mongoose;
 
@@ -42,7 +43,7 @@ const walkerSchema = new Schema({
             ref: review,
         },
       ],
-      earning: [Integer],
+      earning: [Number],
 
       availability: {
         date: {
@@ -106,4 +107,22 @@ const walkerSchema = new Schema({
         }
         
         });
+// set up pre-save middleware to create password
+walkerSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+
+    next();
+});
+
+// compare the incoming password with the hashed password
+walkerSchema.methods.isCorrectPassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
+
+const Walker = mongoose.model('Walker', walkerSchema);
+
+module.exports = Walker;
             
