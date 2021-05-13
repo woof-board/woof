12ee21393c3db238142d4c79a80f01369af11f9a
Nathan Chow <emailme@nathanchow.ca>
2021-05-12 21:26:16 -0400

    update owner/walker pages

--- client/src/components/OwnerSignupForm/index.js
+++ client/src/components/OwnerSignupForm/index.js
@@ -11,11 +11,22 @@ function OwnerSignupForm() {
         <form id="owner-signup-form" className="signup-form">
             <input className="signup-login-input" type="text" placeholder="Enter First Name"></input>
             <input className="signup-login-input" type="text" placeholder="Enter Last Name"></input>
+            <input className="signup-login-input" type="text" placeholder="Enter Address"></input>
+            <input className="signup-login-input" type="text" placeholder="Enter Phone Number"></input>
+            <input className="signup-login-input" type="text" placeholder="Enter Number of Dogs"></input>
             <input className="signup-login-input" type="email" placeholder="Enter Email"></input>
             <input className="signup-login-input" type="text" placeholder="Enter Password"></input>
             <button className="form-button" onClick={ownerSignup} id="owner-signup-button">SUBMIT</button>
         </form>
     )
 }
+
+// firstName
+// lastname
+// email
+// password
+// address
+// phone Number
+// Number of dogs
 
 export default OwnerSignupForm;
