2ed78a8882e8e0d53198decbbc0cf8ee6f629b47
samiul1988 <samiulhaydereee@gmail.com>
2021-05-13 01:32:14 -0600

    added login functionality for both owner and walker

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,41 +1,75 @@
 import gql from 'graphql-tag';
 
 export const ADD_ORDER = gql`
-  mutation addOrder($input: OrderInput!) {
-    addOrder(input: $input) {
-      _id
-      serviceDate
-      serviceTime
-      owner{
-        _id
-      }
-      walker{
-        _id
-      }
+    mutation addOrder($input: OrderInput!) {
+        addOrder(input: $input) {
+            _id
+            serviceDate
+            serviceTime
+            owner{
+                _id
+            }
+            walker{
+                _id
+            }
+        }
     }
-  }
 `;
 
-export const LOGIN_WALKER_USER = gql`
-mutation login($email: String!, $password: String!) {
-  login(email: $email, password: $password) {
-    token
-    user {
-      _id
-      username
+export const ADD_WALKER = gql`
+    mutation addWalker($input: WalkerInput) {
+        addWalker(input: $input) {
+            token
+            walker {
+                _id
+                firstName
+                lastName
+                email
+                ratings
+                averageRating
+            }
+        }
     }
-  }
-}
 `;
 
-export const LOGIN_OWNER_USER = gql`
-mutation login($email: String!, $password: String!) {
-  login(email: $email, password: $password) {
-    token
-    user {
-      _id
-      username
+export const LOGIN_WALKER = gql`
+    mutation loginWalker($email: String!, $password: String!) {
+        loginWalker(email: $email, password: $password) {
+            token
+            walker {
+                _id
+                email
+            }
+        }
     }
-  }
+`;
+
+export const ADD_OWNER = gql`
+    mutation addOwner($input: OwnerInput) {
+        addOwner(input: $input) {
+            token
+            owner {
+                _id
+                firstName
+                lastName
+                email
+                admin
+                dogs {
+                    name
+                }
+            }
+        }
+    }
+`;
+
+export const LOGIN_OWNER = gql`
+    mutation loginOwner($email: String!, $password: String!) {
+        loginOwner(email: $email, password: $password) {
+            token
+            owner {
+                _id
+                email
+            }
+        }
 }
 `;
