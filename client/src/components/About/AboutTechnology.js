14782f65436d0448715419dfb06ad7696a436bec
Nathan Chow <emailme@nathanchow.ca>
2021-05-16 17:17:50 -0400

    about us page styling/components

--- client/src/components/About/AboutTechnology.js
+++ client/src/components/About/AboutTechnology.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
-function AboutTechnology() {
+function AboutTechnology(props) {
+
+    const {
+        safety = [],
+        currentSafeLink,
+        setCurrentSafeLink
+    } = props
+
     return (
         <>
         <div className="flex-tech">
@@ -8,11 +15,19 @@ function AboutTechnology() {
                 <img className="tech-picture" alt="gps" src={process.env.PUBLIC_URL + '/images/about-gps.jpeg'} />
                 <p><b>Our commitment is your safety</b></p>
                 <p>With every safety feature and every standard in our Community Guidelines, we're committed to helping to create a safe environment for our users.</p>
+
+                <div className="flex-center">
+                    <span className={`learnLink ${currentSafeLink.name === 'safety' && `learnActive`}`} onClick={() => { setCurrentSafeLink(safety[1]); }} > Learn More </span>
+                </div>
             </div>
             <div className="tech-card">
                 <img className="tech-picture" alt="gps" src={process.env.PUBLIC_URL + '/images/about-payment.png'} />
                 <p><b>Easy payment options</b></p>
                 <p>Our app is available with the best payment provide, that provides secure, easy and fast payments for our users.</p>
+
+                <div className="flex-center">
+                <span className={`learnLink ${currentSafeLink.name === 'payment' && `learnActive`}`} onClick={() => { setCurrentSafeLink(safety[2]); }} > Learn More </span>  
+                </div>
             </div>
         </div>
 
