ef1c5d510168e9f9e8b01c9c0dce9fa7de286f10
Eric Normann <eric.n@me.com>
2021-05-18 20:52:09 -0400

    finalize home page

--- client/src/components/LoginSIgnupForm/index.js
+++ client/src/components/LoginSIgnupForm/index.js
@@ -4,15 +4,18 @@ import '../../css/LoginSignupForm.css';
 function FormHeader(props) {
 
     const {
+        currentLink,
         formLinks = [],
         setFormCurrentLink,
-        currentFormLink
+        currentFormLink,
     } = props
 
+    console.log(currentLink)
+
     return (
-        <div className="form-title">
+        <div className={`form-title ${currentLink.action}`}>
             {formLinks.map((link) => (
-            <span key={link.name} className={`form-link ${currentFormLink.name === link.name && `formActive`}`} onClick={() => { setFormCurrentLink(link);}} id={link.id}>{link.name}</span>
+            <h3 key={link.name} className={`form-link ${currentFormLink.name === link.name && `formActive`}`} onClick={() => { setFormCurrentLink(link);}} id={link.id}>{link.name}</h3>
             ))}                 
         </div>
     )
