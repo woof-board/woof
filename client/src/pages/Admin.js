c49bcd7e5f82566010558dd97fdc1ee0aba92f47
Alex Chung <alex.chung92@gmail.com>
2021-05-21 09:57:20 -0400

    add admin data

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react';
 import { useLazyQuery } from '@apollo/react-hooks';
-import { Admin, Resource } from 'react-admin';
+import React, { Component } from 'react';
+import { Admin, Resource, ListGuesser } from 'react-admin';
 import { Create, UrlField, DisabledInput, SimpleForm, TextInput } from 'react-admin';
 import '../css/WalkerProfile.css';
 import WalkerDetails from '../components/WalkerProfileComponents/WalkerDetails';
@@ -11,16 +11,36 @@ import WalkerEarnings from '../components/WalkerProfileComponents/WalkerEarnings
 import WalkerAvgRating from '../components/WalkerProfileComponents/WalkerAvgRating';
 
 import OwnerDetails from '../components/OwnerProfile/OwnerDetails';
+import jsonServerProvider from "ra-data-json-server";
 
+//const dataProvider = {WalkerDetails, WalkerReviews, WalkerOrders, WalkerEarnings, WalkerAvgRating, OwnerDetails};
+const dataProvider =
+  jsonServerProvider("https://jsonplaceholder.typicode.com");
+//import React, { Component } from 'react';
+//import AdminHeader from './components/AdminHeader';
+//import SideBar from './components/SideBar';
+//import Content from './components/AdminContent';
 
-const dataProvider = {WalkerDetails, WalkerReviews, WalkerOrders, WalkerEarnings, WalkerAvgRating, OwnerDetails};
+//class AdminPage extends Component {
+
+//  render() {
+//    return (
+//      <div>
+//        <AdminHeader />
+//        <SideBar />
+//        <Content />
+//      </div>
+//    );
+//  }
+//}
 
 
 
 function AdminPage() {  
     return (
-      <Admin dataProvider={dataProvider}/>
-
+      <Admin dataProvider={dataProvider}>
+        <Resource name="users" list={ListGuesser} />
+      </Admin>
     );
 }
 
